1bc0022d522b95344d79112a771f81ae5b069f5c
Bogdan Pyrskij <pyrskij@list.ru>
2024-02-21 13:56:40 +0300

    first commit

--- app.vue
+++ app.vue
@@ -1,6 +1,16 @@
 <template>
-  <NuxtLayout>
- 
-  </NuxtLayout>
+  <NuxtPage />
 </template>
+
+<style>
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.5s ease;
+}
+
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
+}
+</style>
 
